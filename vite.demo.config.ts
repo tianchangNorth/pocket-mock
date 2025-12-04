5c428c4a00fad608969a73b44d0d3f7acd19e32e
tianchang <tianchang488@gmail.com>
2025-12-04 13:52:44 +0800

    fix :Code Structure Optimization

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -2,21 +2,18 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ⚠️ 注意：这里必须换成你的仓库名，例如 '/pocket-mock/'
-  // 如果你的仓库名就是 pocket-mock，这里不用改
-  base: './', 
+  base: './',
   plugins: [
     svelte()
   ],
   resolve: {
     alias: {
-      'pocket-mocker': resolve(__dirname, './src/index.ts') // 让 Demo 引用本地源码
+      'pocket-mocker': resolve(__dirname, './src/index.ts')
     }
   },
   build: {
-    outDir: 'demo-dist', // 输出到专门的演示目录
+    outDir: 'demo-dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
